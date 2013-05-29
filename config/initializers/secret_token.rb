5ddb4b1a94b071d3feeabe9ae9a60007e00ac6be
Kevin Copley <kpcmm@yahoo.com>
2013-05-29 07:35:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbp::Application.config.secret_token = 'd32725e725870d8bd22f9595aaa9bed12bfcc0fe31e465c57c2750876f36f461f64efd0ab4eab7f71c5ca5afc966f4a9a2b717d13497ac0578b3352997463ff2'
