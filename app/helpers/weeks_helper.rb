0cb561e664abb1d477169e2b0f8e9d1c41a032f5
Kevin Copley <kpcmm@yahoo.com>
2013-09-10 15:30:14 -0700

    enable some more status 3

--- app/helpers/weeks_helper.rb
+++ app/helpers/weeks_helper.rb
@@ -293,14 +293,14 @@ module WeeksHelper
 		    r.draw canvas
 		  end
 
-		  `ls -l app/assets/images'`.each_line { |line| @status << "find: #{line}"}
+		  `ls -l app/assets/images`.each_line { |line| @status << "find: #{line}"}
 		  status << "writing image"
 		  image_file = "app/assets/images/result_#{week.week_num}_#{current_user.name}.png"
 		  `rm -f #{image_file}`
 		  canvas.write image_file
 
 		  status << "image done"
-		  `ls -l app/assets/images'`.each_line { |line| @status << "find: #{line}"}
+		  `ls -l app/assets/images`.each_line { |line| @status << "find: #{line}"}
 		  status
 	end
 
