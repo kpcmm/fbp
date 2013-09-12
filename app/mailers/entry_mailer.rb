701df05baabca7dc8f21ae60d6605c68b03e9db8
Kevin Copley <kpcmm@yahoo.com>
2013-09-11 19:53:11 -0700

    fbpceo@gmail.com

--- app/mailers/entry_mailer.rb
+++ app/mailers/entry_mailer.rb
@@ -8,7 +8,8 @@ class EntryMailer < ActionMailer::Base
   	@week = entry.week
   	cc = 'kpcmm@yahoo.com' if Rails.env.development?
   	cc = 'kpcmm@yahoo.com' if Rails.env.test?
-  	cc = 'fbphq@yahoo.com' if Rails.env.production?
+    cc = 'fbphq@yahoo.com' if Rails.env.production?
+    bc = 'fbpceo@gmail.com' if Rails.env.production?
   	mail(to: @user.email, subject: "Your FBPhq week #{@week.week_num} entry", cc: cc)
   end
 end
