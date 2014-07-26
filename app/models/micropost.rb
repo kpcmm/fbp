b664a527d16ffa79e93ae3d200abfb8f8fd42805
Kevin Copley <kpcmm@yahoo.com>
2014-07-26 05:58:12 -0700

    fix microposts default scope error 2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,4 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   belongs_to :user
-  default_scope  { order: 'microposts.created_at DESC' }
 end
